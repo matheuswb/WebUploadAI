1a59badf728073800b6d49de8b8ee8be01bd349e
Matheus Willian Bento <35368739+matheuswb@users.noreply.github.com>
2023-09-29 19:33:08 -0300

    Prompt e Button

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { Label } from './components/ui/label';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './components/ui/select';
 import { Slider } from './components/ui/slider';
 import { VideoInputForm } from './components/video-input-form';
+import { PromptSelect } from './components/prompt-select';
 
 export function App() {
   return (
@@ -51,15 +52,7 @@ export function App() {
           <form className="space-y-6">
             <div className="space-y-2">
               <Label>Prompt</Label>
-              <Select>
-                <SelectTrigger>
-                  <SelectValue placeholder="Selecione um prompt..."/>  
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="title">Título do Youtube</SelectItem>
-                  <SelectItem value="description">Descrição do Youtube</SelectItem>                  
-                </SelectContent>
-              </Select>
+              <PromptSelect />
             </div>
 
             <div className="space-y-2">
